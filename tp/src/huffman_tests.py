1df626b317bc7c8da6b1669c14cbede3b5965b6b
Onixyz <goulven.ruellan@gmail.com>
2022-03-30 02:25:51 +0200

    oauis commit

--- tp/src/huffman_tests.py
+++ tp/src/huffman_tests.py
@@ -1,13 +1,44 @@
 import unittest
+import huffman
 
 class TestFileMethods(unittest.TestCase):
+    
 
+    def test_content_little_file(self):
+        filename="littleFile.txt"
+        filename_encoded="littleFileEncoded.bin"
+        F=huffman.frequences()
+        tree = huffman.arbre_huffman(F)
 
-    def test_size(self):
-        self.assertTrue(True)
+        decoded = huffman.decompress_from_file(tree, filename_encoded)
 
-    def test_content(self):
-        self.assertEqual("a","a")
+        with open(filename,'r', encoding="utf-8") as f:
+            clear = f.read()
+        self.assertEqual(clear,decoded)
+
+    def test_content_big_file(self):
+        filename="bigFile.txt"
+        filename_encoded="bigFileEncoded.bin"
+        F=huffman.frequences()
+        tree = huffman.arbre_huffman(F)
+
+        decoded = huffman.decompress_from_file(tree, filename_encoded)
+
+        with open(filename,'r', encoding="utf-8") as f:
+            clear = f.read()
+        self.assertEqual(clear,decoded)
+
+    def test_content_leHorla(self):
+        filename="leHorla.txt"
+        filename_encoded="leHorlaEncoded.bin"
+        F=huffman.frequences()
+        tree = huffman.arbre_huffman(F)
+
+        decoded = huffman.decompress_from_file(tree, filename_encoded)
+        
+        with open(filename,'r', encoding="utf-8") as f:
+            clear = f.read()
+        self.assertEqual(clear,decoded)
 
 if __name__ == '__main__':
     unittest.main()
--- tp/src/tests.py
+++ tp/src/tests.py
@@ -1,18 +1,55 @@
 #test.py
-import unittest
 import huffman_nofreq
-import sys
+import huffman
+import os
 
-class TestLengthFile(unittest.TestCase):
+def createFileEncoded_nofreq(filename,filename_encoded):
     
-    def test_negativeForLess(self):
-        size_compressed = 6
-        size_initial = 5
-        # error message in case if test case got failed
-        message = "Le fichier compressé est plus grand que le fichier initial."
-          
-        # assert function() to check if values1 is less than value2
-        self.assertLess(size_compressed, size_initial, message)
+    with open(filename,'r', encoding="utf-8") as f:
+        clear = f.read()
+
+    #Compression du fichier
+    huffman_nofreq.compress_to_file(clear,filename_encoded)
+
+def createFileEncoded(filename):
+    F=huffman.frequences()
+    tree = huffman.arbre_huffman(F)
+
+    huffman.compress_to_file(tree, filename)
+
+    
+
+
+def compareSize(filename,filename_encoded):
+    size_compressed = os.path.getsize(filename_encoded)
+    size_initial = os.path.getsize(filename)
+    compression_rate = int(((size_initial-size_compressed)/size_initial)*100)
+    print("Le fichier initial (",filename,") a une taille de : ",size_initial,"octets")
+    print("Le fichier compressé a une taille de : ",size_compressed,"octets")
+    print("Le taux de compression est de :",compression_rate)
+    print("\n")
+    
 
 if __name__ == '__main__':
-    unittest.huffman_nofreq.main()
+
+    createFileEncoded_nofreq("littleFile.txt","littleFileEncoded.bin")
+    createFileEncoded_nofreq("bigFile.txt","bigFileEncoded.bin")
+    createFileEncoded_nofreq("leHorla.txt","leHorlaEncoded.bin")
+
+    print("Comparaison des tailles de fichiers avec l'utilisation de l'algorithme d'huffman sans fréquence :")
+    compareSize("littleFile.txt","littleFileEncoded.bin")
+    compareSize("bigFile.txt","bigFileEncoded.bin")
+    compareSize("leHorla.txt","leHorlaEncoded.bin")
+
+
+    createFileEncoded("littleFile.txt")
+    createFileEncoded("bigFile.txt")
+    createFileEncoded("leHorla.txt")
+    
+    print("Comparaison des tailles de fichiers avec l'utilisation de l'algorithme d'huffman avec fréquence :")
+    compareSize("littleFile.txt","littleFileEncoded.bin")
+    compareSize("bigFile.txt","bigFileEncoded.bin")
+    compareSize("leHorla.txt","leHorlaEncoded.bin")
+
+    
+
